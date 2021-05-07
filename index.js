77b8e1320f1acb815827a2ac43f42a89a72db069
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-07 11:37:38 -0500

    Classes and index.js updated

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+function promptUser() {
+    
+}
+
+function generateHtml() {
+
+}
